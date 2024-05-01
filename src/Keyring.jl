ed78b9d72b6f705fdd3cb8052a2a735d1e332dfa
Alec Loudenback <alecloudenback@gmail.com>
2024-04-30 23:18:03 -0500

    strict docs

--- src/Keyring.jl
+++ src/Keyring.jl
@@ -3,6 +3,16 @@ module Keyring
 include("core.jl")
 include("Windows.jl")
 
+"""
+    DEFAULT_CREDENTIAL_STORE()::T<:AbstractCredentialStore
+
+Returns the default credential store for the current system.
+
+- Windows: [`Windows.WindowsCredentialManager`](@ref)
+- macOS: TBD
+- Linux: TBD
+
+"""
 DEFAULT_CREDENTIAL_STORE() =
     if Sys.islinux()
         # TODO
